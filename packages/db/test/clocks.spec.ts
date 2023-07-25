912631ad0f0ca44ec12ba6afede66740d7b82e2f
pbohlman <phil.l.bohlman.gr@dartmouth.edu>
2023-07-25 18:37:50 -0400

    add tests for durable clock

--- packages/db/test/clocks.spec.ts
+++ packages/db/test/clocks.spec.ts
@@ -33,4 +33,54 @@ describe('DurableClock', () => {
     expect(currentTs[0]).toBe(15);
     expect(currentTs[1]).toBe('alice');
   });
+  it('can get the next timestamp', async () => {
+    const clock = new DurableClock('store');
+    new TripleStore({
+      storage: { store: new MemoryStorage() },
+      clock,
+    });
+    const currentTick = (await clock.getCurrentTimestamp())[0];
+    expect(currentTick).toBe(0);
+    const nextTick = (await clock.getNextTimestamp())[0];
+    expect(nextTick).toBe(1);
+  });
+  it('can set the clock', async () => {
+    const clock = new DurableClock('store');
+    new TripleStore({
+      storage: { store: new MemoryStorage() },
+      clock,
+    });
+    let currentTick = (await clock.getCurrentTimestamp())[0];
+    expect(currentTick).toBe(0);
+    await clock.setTick(15);
+    currentTick = (await clock.getCurrentTimestamp())[0];
+    expect(currentTick).toBe(15);
+  });
+  it('can read new timestamps from the store and update the clock', async () => {
+    const clock = new DurableClock('store');
+    const db = new TripleStore({
+      storage: { store: new MemoryStorage() },
+      clock,
+    });
+    let currentTick = await clock.getCurrentTimestamp();
+    expect(currentTick[0]).toBe(0);
+    await db.transact(async (tx) => {
+      await tx.insertTriple({
+        id: 'a',
+        attribute: ['b'],
+        value: 'c',
+        expired: false,
+        timestamp: [16, currentTick[1]],
+      });
+    });
+    currentTick = await clock.getCurrentTimestamp();
+    expect(currentTick[0]).toBe(16);
+  });
+  it.todo(
+    "throws an error when it's assigned to something other than a TripleStore",
+    async () => {
+      const clock = new DurableClock('store');
+      expect(await clock.assignToStore({} as any)).toThrow();
+    }
+  );
 });
