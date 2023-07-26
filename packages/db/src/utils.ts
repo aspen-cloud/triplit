dc642ffcb673d004d9a821aa7da726646ff4549b
pbohlman <phil.l.bohlman.gr@dartmouth.edu>
2023-07-26 14:08:28 -0400

    rm unused utils

--- packages/db/src/utils.ts
+++ packages/db/src/utils.ts
@@ -28,37 +28,6 @@ export function objectToTuples(
   );
 }
 
-export function pathValsToObj(pathVals: any[][]) {
-  return pathVals.reduce(updateObjWithPathVal, {});
-}
-
-function updateObjWithPathVal(obj: any, pathVal: any[]) {
-  const paths = pathVal.slice(0, -1);
-  const value = pathVal.at(-1);
-  let scope = obj;
-  for (let i = 0; i < paths.length; i++) {
-    const key = paths[i];
-    if (i === paths.length - 1) {
-      scope[key] = value;
-      continue;
-    }
-    // if key already exists just update scope
-    if (scope[key] !== undefined) {
-      scope = scope[key];
-      continue;
-    }
-    const nextKey = paths[i + 1];
-    // heuristic for is an array
-    if (nextKey === 0) {
-      scope[key] = [];
-    } else {
-      scope[key] = {};
-    }
-    scope = scope[key];
-  }
-  return obj;
-}
-
 export function triplesToObject<T>(triples: TuplePrefix<EAV>[]) {
   const result: any = {};
   for (const [e, a, v] of triples) {
