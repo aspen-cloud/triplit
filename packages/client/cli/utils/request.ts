cb8eda113dc69d7b423634802c739461cf8b9c56
Matthew Linkous <matthew.linkous@gmail.com>
2023-08-18 09:51:53 -0400

    Fix CLI https issues

--- packages/client/cli/utils/request.ts
+++ packages/client/cli/utils/request.ts
@@ -2,6 +2,13 @@ import dns from 'node:dns';
 import http from 'node:http';
 import fetch, { RequestInit } from 'node-fetch';
 
+/**
+ * This is a custom agent that will resolve custom subdomains on localhost
+ * e.g. http://myprojectid.localhost:8787
+ *
+ * Caveat: It doesn't currently support HTTPS I think it would need
+ * to use a separate node:https agent which isn't currently needed
+ */
 const agent = new http.Agent({
   lookup: (hostname, options, callback) => {
     if (hostname.includes('localhost')) {
@@ -15,6 +22,8 @@ const agent = new http.Agent({
 });
 
 export async function request(path: string, options?: RequestInit) {
-  console.log(path, options);
-  return fetch(path, { agent, ...options });
+  if (path.startsWith('http://')) {
+    return fetch(path, { agent, ...options });
+  }
+  return fetch(path, { ...options });
 }
