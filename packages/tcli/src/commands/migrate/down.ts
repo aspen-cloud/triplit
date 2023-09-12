df47063e6aaeda150b93cedeb6c0ca2642bead13
Matthew Linkous <matthew.linkous@gmail.com>
2023-09-12 13:57:58 -0500

    Link CLI and Console in private packages

--- packages/tcli/src/commands/migrate/down.ts
+++
@@ -1,7 +0,0 @@
-import { yellow, italic } from 'ansis/colors';
-
-export const description = 'Runs down migrations on the database';
-
-export const run = async () => {
-  console.log(yellow`Running ${italic('down')} migrations`);
-};
--- packages/tcli/src/commands/migrate/up.ts
+++
@@ -1,8 +0,0 @@
-import { blue, italic } from 'ansis/colors';
-
-export const description = 'Runs up migrations on the database';
-
-export const run = async () => {
-  // @ts-ignore
-  console.log(blue`Running ${italic('up')} migrations`);
-};
