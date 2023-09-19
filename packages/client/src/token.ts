004889796e6152851b5d0bd9d58a3525275603dd
Matthew Linkous <matthew.linkous@gmail.com>
2023-09-19 13:00:25 -0500

    Remove unnecessary window reference

--- packages/client/src/token.ts
+++ packages/client/src/token.ts
@@ -20,8 +20,7 @@ function decodeJwt(token: string) {
   var base64Url = token.split('.')[1];
   var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
   var jsonPayload = decodeURIComponent(
-    window
-      .atob(base64)
+    atob(base64)
       .split('')
       .map(function (c) {
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
