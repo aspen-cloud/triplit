cdd381195c95fae16c1b1c90580c3ad7c822d5a0
pbohlman <phil.l.bohlman.gr@dartmouth.edu>
2023-10-25 10:49:26 -0500

    use indexedDb in consoleClient

--- packages/console/triplit/client.ts
+++ packages/console/triplit/client.ts
@@ -1,5 +1,6 @@
 import { schemaObject as DBConsoleSchemaObject } from './schema';
 import { TriplitClient } from '@triplit/client';
+import { IndexedDbStorage } from '@triplit/db/storage/indexed-db';
 
 export function formConsolePrimaryKey(projectId: string, server: string) {
   return `${projectId}-${server}`;
@@ -8,5 +9,6 @@ export function formConsolePrimaryKey(projectId: string, server: string) {
 export const consoleClient = new TriplitClient({
   db: {
     schema: { collections: DBConsoleSchemaObject, version: 0 },
+    storage: { cache: new IndexedDbStorage(), outbox: new IndexedDbStorage() },
   },
 });
