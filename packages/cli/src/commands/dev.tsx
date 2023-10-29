6192791cab88c590eb257d815b448a60e9826ac8
Matthew Linkous <matthew.linkous@gmail.com>
2023-10-29 11:30:15 -0500

    Migrate dev command

--- packages/cli/src/commands/dev.tsx
+++ packages/cli/src/commands/dev.tsx
@@ -6,104 +6,123 @@ import jwt from 'jsonwebtoken';
 import path from 'path';
 import fs from 'fs';
 import { getDataDir } from '../filesystem.js';
+import { Command } from '../command.js';
 
-export const description = 'Starts the Triplit development environment';
-export const flags = {};
-export const args = {};
+export default Command({
+  description: 'Starts the Triplit development environment',
+  flags: {
+    storage: {
+      type: 'string',
+      char: 's',
+      description: 'Database storage type',
+    },
+    consolePort: {
+      type: 'number',
+      char: 'c',
+      description: 'Port to run the console server on',
+    },
+    dbPort: {
+      type: 'number',
+      char: 'd',
+      description: 'Port to run the database server on',
+    },
+  },
+  async run({ flags }) {
+    const consolePort = flags.consolePort || 6542;
+    const dbPort = flags.dbPort || 6543;
 
-export async function run({ flags }) {
-  const consolePort = flags.consolePort || 6542;
-  const dbPort = flags.dbPort || 6543;
+    process.env.JWT_SECRET =
+      process.env.JWT_SECRET ?? 'jwt-key-for-development-only';
+    process.env.PROJECT_ID = process.env.PROJECT_ID ?? 'local-project-id';
 
-  process.env.JWT_SECRET =
-    process.env.JWT_SECRET ?? 'jwt-key-for-development-only';
-  process.env.PROJECT_ID = process.env.PROJECT_ID ?? 'local-project-id';
-
-  if (flags.storage === 'sqlite') {
-    const dataDir = getDataDir();
-    const sqlitePath = path.join(dataDir, 'sqlite', 'app.db');
-    if (!fs.existsSync(path.dirname(sqlitePath))) {
-      fs.mkdirSync(path.dirname(sqlitePath), { recursive: true });
+    if (flags.storage === 'sqlite') {
+      const dataDir = getDataDir();
+      const sqlitePath = path.join(dataDir, 'sqlite', 'app.db');
+      if (!fs.existsSync(path.dirname(sqlitePath))) {
+        fs.mkdirSync(path.dirname(sqlitePath), { recursive: true });
+      }
+      process.env.LOCAL_DATABASE_URL = sqlitePath;
     }
-    process.env.LOCAL_DATABASE_URL = sqlitePath;
-  }
 
-  const serviceKey = jwt.sign(
-    {
-      'x-triplit-token-type': 'secret',
-      'x-triplit-project-id': process.env.PROJECT_ID,
-    },
-    process.env.JWT_SECRET
-  );
+    const serviceKey = jwt.sign(
+      {
+        'x-triplit-token-type': 'secret',
+        'x-triplit-project-id': process.env.PROJECT_ID,
+      },
+      process.env.JWT_SECRET
+    );
 
-  const anonKey = jwt.sign(
-    {
-      'x-triplit-token-type': 'anon',
-      'x-triplit-project-id': process.env.PROJECT_ID,
-    },
-    process.env.JWT_SECRET
-  );
+    const anonKey = jwt.sign(
+      {
+        'x-triplit-token-type': 'anon',
+        'x-triplit-project-id': process.env.PROJECT_ID,
+      },
+      process.env.JWT_SECRET
+    );
 
-  const startDBServer = createDBServer({ storage: flags.storage || 'memory' });
-  const dbServer = startDBServer(dbPort);
-  const consoleServer = createConsoleServer('../../console', {
-    token: serviceKey,
-    projName: 'triplit-test',
-    server: `http://localhost:${dbPort}`,
-  });
-  consoleServer.listen(consolePort);
+    const startDBServer = createDBServer({
+      storage: flags.storage || 'memory',
+    });
+    const dbServer = startDBServer(dbPort);
+    const consoleServer = createConsoleServer('../../console', {
+      token: serviceKey,
+      projName: 'triplit-test',
+      server: `http://localhost:${dbPort}`,
+    });
+    consoleServer.listen(consolePort);
 
-  process.on('SIGINT', function () {
-    dbServer.close();
-    consoleServer.close();
-    process.exit();
-  });
+    process.on('SIGINT', function () {
+      dbServer.close();
+      consoleServer.close();
+      process.exit();
+    });
 
-  return (
-    <>
-      <Newline />
-      <Box flexDirection="column" gap={1}>
-        <Text bold underline color={'magenta'}>
-          Triplit Development Environment
-        </Text>
-        <Box flexDirection="column">
-          <Text>
-            You can access your local Triplit services at the following local
-            URLs:
-          </Text>
-          <Box
-            width={48}
-            flexDirection="column"
-            borderStyle="single"
-            paddingX={1}
-          >
-            <Box>
-              <Text bold>🟢 Console</Text>
-              <Spacer />
-              <Text color="cyan">{`http://localhost:${consolePort}`}</Text>
-            </Box>
-            <Box>
-              <Text bold>🟢 Database</Text>
-              <Spacer />
-              <Text color="cyan">{`http://localhost:${dbPort}`}</Text>
-            </Box>
-          </Box>
-        </Box>
+    return (
+      <>
+        <Newline />
         <Box flexDirection="column" gap={1}>
+          <Text bold underline color={'magenta'}>
+            Triplit Development Environment
+          </Text>
           <Box flexDirection="column">
-            <Text bold underline>
-              Service Key
+            <Text>
+              You can access your local Triplit services at the following local
+              URLs:
             </Text>
-            <Text wrap="end">{serviceKey}</Text>
+            <Box
+              width={48}
+              flexDirection="column"
+              borderStyle="single"
+              paddingX={1}
+            >
+              <Box>
+                <Text bold>🟢 Console</Text>
+                <Spacer />
+                <Text color="cyan">{`http://localhost:${consolePort}`}</Text>
+              </Box>
+              <Box>
+                <Text bold>🟢 Database</Text>
+                <Spacer />
+                <Text color="cyan">{`http://localhost:${dbPort}`}</Text>
+              </Box>
+            </Box>
           </Box>
-          <Box flexDirection="column">
-            <Text bold underline>
-              Anon Token
-            </Text>
-            <Text wrap="end">{anonKey}</Text>
+          <Box flexDirection="column" gap={1}>
+            <Box flexDirection="column">
+              <Text bold underline>
+                Service Key
+              </Text>
+              <Text wrap="end">{serviceKey}</Text>
+            </Box>
+            <Box flexDirection="column">
+              <Text bold underline>
+                Anon Token
+              </Text>
+              <Text wrap="end">{anonKey}</Text>
+            </Box>
           </Box>
         </Box>
-      </Box>
-    </>
-  );
-}
+      </>
+    );
+  },
+});
