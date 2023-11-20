a3c4bca1ab0a325d9d26cda8e94286b9d9b006f5
Will Ernst <wernst@users.noreply.github.com>
2023-11-20 15:47:57 -0800

    return proper type for single entity subscription

--- packages/db/src/collection-query.ts
+++ packages/db/src/collection-query.ts
@@ -142,7 +142,8 @@ export async function fetch<
     const triples = new Map([[queryWithInsertedVars.entityId, entityTriples]]);
     if (!entity || !doesEntityObjMatchWhere(entity, where, collectionSchema)) {
       const results = new Map() as FetchResult<Q>;
-      return includeTriples ? { results, triples: [] } : results;
+      const triples = new Map() as Map<string, TripleRow[]>;
+      return includeTriples ? { results, triples } : results;
     }
     let updatedEntity = entity;
     // @WE I'm sure theres a reason, but its unclear why we do this? If we arent including triples, dont strip timestamps?
