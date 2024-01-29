d54cc1407db3ada3c7fc9efe300418300d589a7d
Will Ernst <wernst@users.noreply.github.com>
2024-01-29 10:34:40 -0800

    export types only for server sync messages

--- packages/server-core/src/index.ts
+++ packages/server-core/src/index.ts
@@ -1,4 +1,4 @@
-export * from '@triplit/types/sync';
+export type * from '@triplit/types/sync.js';
 export { Server } from './triplit-server.js';
 export * from './session.js';
 export * from './errors.js';
