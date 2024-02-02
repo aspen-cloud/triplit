caecfc9373d493b16ebc3cf64dcdbbeae33d037c
pbohlman <phil.l.bohlman.gr@dartmouth.edu>
2024-02-02 14:03:59 -0600

    cli: check for local supabase secrets for local dev

--- packages/cli/src/supabase.ts
+++ packages/cli/src/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  'https://dmghtipnbmxwqtzpuktx.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtZ2h0aXBuYm14d3F0enB1a3R4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE0OTA0MDUsImV4cCI6MTk5NzA2NjQwNX0.ZjFKaBx-RVqPr58TyCCQnCrcYgy3zGNUjoU_ZmJDeqI'
+  process.env.SUPABASE_URL ?? 'https://dmghtipnbmxwqtzpuktx.supabase.co',
+  process.env.SUPABASE_ANON_KEY ??
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtZ2h0aXBuYm14d3F0enB1a3R4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE0OTA0MDUsImV4cCI6MTk5NzA2NjQwNX0.ZjFKaBx-RVqPr58TyCCQnCrcYgy3zGNUjoU_ZmJDeqI'
 );
