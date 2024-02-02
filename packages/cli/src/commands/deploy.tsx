1c3934dd192285b12f98a8b7087960153cf3ec50
Matthew Linkous <matthew.linkous@gmail.com>
2024-02-02 17:28:51 -0600

    Setup proper bundle uploading and dispatching

--- packages/cli/src/commands/deploy.tsx
+++ packages/cli/src/commands/deploy.tsx
@@ -2,7 +2,7 @@ import { Command } from '../command.js';
 import * as Flag from '../flags.js';
 import * as esbuild from 'esbuild';
 import { fileURLToPath } from 'url';
-import path from 'path';
+import * as path from 'path';
 import axios, { AxiosError } from 'axios';
 import { accessTokenMiddleware } from '../middleware/account-auth.js';
 
@@ -13,6 +13,7 @@ export default Command({
   flags: {
     projectId: Flag.String({
       description: 'Project ID',
+      required: true,
     }),
     triplitDir: Flag.String({
       description: 'Triplit directory',
@@ -26,6 +27,8 @@ export default Command({
       '../deploy/worker.js'
     );
     const result = await esbuild.build({
+      target: 'es2022',
+      format: 'esm',
       entryPoints: [workerPath],
       bundle: true,
       // outfile: './triplit/.deploy/index.js',
@@ -40,12 +43,10 @@ export default Command({
       }
       return;
     }
-    const resultBytes = result.outputFiles[0].contents;
-    const resultTextBase64 = Buffer.from(resultBytes).toString('base64');
     try {
       const response = await axios.post(
         `http://localhost:8787/deploy/${flags.projectId}`,
-        resultTextBase64,
+        result.outputFiles[0].text,
         {
           headers: {
             'Content-Type': 'text/plain',
@@ -58,13 +59,13 @@ export default Command({
       console.log('Could not upload code bundle.');
       if (err instanceof AxiosError) {
         // log info about Axios Error
-        if (err.request) {
-          console.log(err.code);
-          return;
-        }
         if (err.response) {
           console.log(err.response.status);
           console.log(err.response.data);
+          return;
+        }
+        if (err.request) {
+          console.log(err.code);
           return;
         }
       }
