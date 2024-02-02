31ee3e12c609db25325322ae6d1791d8637b6431
pbohlman <phil.l.bohlman.gr@dartmouth.edu>
2024-02-02 14:02:54 -0600

    CLI: add server requester to auth middleware

--- packages/cli/src/middleware/account-auth.ts
+++ packages/cli/src/middleware/account-auth.ts
@@ -1,6 +1,9 @@
 import { getSession, storeSession } from '../auth-state.js';
 import { Middleware } from '../middleware.js';
 import { supabase } from '../supabase.js';
+import axios from 'axios';
+
+const MANAGEMENT_API = 'http://localhost:8787';
 
 export const accessTokenMiddleware = Middleware({
   name: 'Access Token',
@@ -20,8 +23,43 @@ export const accessTokenMiddleware = Middleware({
       session = data.session;
       storeSession(data.session);
     }
+    const requestServer = makeAPIRequester(session.access_token);
     return {
       accessToken: session.access_token,
+      session,
+      requestServer,
     };
   },
 });
+
+function makeAPIRequester(token: string) {
+  return async function request(
+    method: 'GET' | 'POST',
+    path: string,
+    params?: any
+  ) {
+    const payload = method === 'GET' ? { params } : { data: params };
+    const url = MANAGEMENT_API + path;
+    try {
+      const resp = await axios.request({
+        method,
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+          authorization: `Bearer ${token}`,
+        },
+        ...payload,
+      });
+
+      // axios will throw on non-2xx status codes: https://axios-http.com/docs/handling_errors
+      return resp.data;
+    } catch (e: any) {
+      if (e.response) {
+        throw e.response.data;
+      } else if (e.request) {
+        throw `No response was received from server: ${url}. Please ensure you are connected to the internet and are pointing to the correct server.`;
+      }
+      throw `An error occured while requesting the remote database: ${e.message}`;
+    }
+  };
+}
