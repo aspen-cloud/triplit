1c2a8940bce6f53c3f0d77527e1b6222b1120605
pbohlman <phil.l.bohlman.gr@dartmouth.edu>
2024-03-08 09:17:49 -0600

    cli: add clear command

---
+++ .changeset/fuzzy-panthers-fix.md
@@ -0,0 +1,5 @@
+---
+'@triplit/cli': patch
+---
+
+add clear remote db command
---
+++ packages/cli/src/commands/clear.ts
@@ -0,0 +1,32 @@
+import { blue } from 'ansis/colors';
+import { serverRequesterMiddleware } from '../middleware/add-server-requester.js';
+import { Command } from '../command.js';
+import prompts from 'prompts';
+import * as Flag from '../flags.js';
+
+export default Command({
+  description: 'Runs up migrations on the remote database',
+  middleware: [serverRequesterMiddleware],
+  flags: {
+    full: Flag.Boolean({
+      char: 'f',
+      description: 'Create a new seed file',
+      default: false,
+    }),
+  },
+  run: async ({ args, ctx, flags }) => {
+    const { proceed } = await prompts({
+      type: 'confirm',
+      name: 'proceed',
+      message: 'Are you sure you want to clear the database?',
+    });
+    if (!proceed) return;
+    console.log(`Clearing the sync server: `, blue(ctx.url));
+    console.log();
+
+    await ctx.requestServer('POST', '/clear', {
+      full: flags.full,
+    });
+    console.log(blue('Database has been cleared'));
+  },
+});
