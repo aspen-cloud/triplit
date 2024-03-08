fc1416719ab0b5d5ada0831701c3e8f158e65dec
Matthew Linkous <matthew.linkous@gmail.com>
2024-03-08 10:17:56 -0600

    DB: Update durable object to write in 120 tuples batches

--- packages/db/src/storage/durable-object-tuple-store.ts
+++ packages/db/src/storage/durable-object-tuple-store.ts
@@ -7,6 +7,10 @@ import type {
 import { decodeTuple, encodeTuple } from '@triplit/tuple-database';
 import type { DurableObjectStorage } from '@cloudflare/workers-types';
 
+// Max 128 keys per put
+// https://developers.cloudflare.com/durable-objects/api/transactional-storage-api/#put
+const WRITE_LIMIT = 120;
+
 export default class DurableObjectStore implements AsyncTupleStorageApi {
   doStore: DurableObjectStorage;
   constructor(doStore: DurableObjectStorage) {
@@ -33,25 +37,39 @@ export default class DurableObjectStore implements AsyncTupleStorageApi {
   async commit(writes: WriteOps<KeyValuePair>): Promise<void> {
     const { set, remove } = writes;
     if (set && set.length > 0) {
-      await this.doStore.put(
-        Object.fromEntries(
-          set?.map(({ key, value }) => [encodeTuple(key), value])
-        ),
-        {
-          allowConcurrency: true,
-          allowUnconfirmed: true,
-        }
-      );
+      await applyInChunks(set, WRITE_LIMIT, async (chunk) => {
+        await this.doStore.put(
+          Object.fromEntries(
+            chunk.map(({ key, value }) => [encodeTuple(key), value])
+          ),
+          {
+            allowConcurrency: true,
+            allowUnconfirmed: true,
+          }
+        );
+      });
     }
     if (remove && remove.length > 0) {
-      await this.doStore.delete(
-        remove.map((tupleKey) => encodeTuple(tupleKey)),
-        {
-          allowConcurrency: true,
-          allowUnconfirmed: true,
-        }
-      );
+      await applyInChunks(remove, WRITE_LIMIT, async (chunk) => {
+        await this.doStore.delete(
+          chunk.map((tupleKey) => encodeTuple(tupleKey)),
+          {
+            allowConcurrency: true,
+            allowUnconfirmed: false,
+          }
+        );
+      });
     }
   }
   async close(): Promise<void> {}
+}
+
+async function applyInChunks<T>(
+  items: T[],
+  chunk_size: number,
+  callback: (chunk: T[]) => Promise<void>
+) {
+  for (let i = 0; i < items.length; i += chunk_size) {
+    await callback(items.slice(i, i + chunk_size));
+  }
 }
