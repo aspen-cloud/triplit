aedea21289c83f8f7a5cf14001ebde95594a143e
Will Ernst <wernst@users.noreply.github.com>
2024-04-16 16:53:44 -0700

    add routing in management api to create projects, fallback to org defaults as needed for project version

--- packages/cli/src/commands/project/create.ts
+++ packages/cli/src/commands/project/create.ts
@@ -14,13 +14,13 @@ export default Command({
   middleware: [accessTokenMiddleware, organizationMiddleware],
   async run({ ctx }) {
     const { organization } = ctx;
-    const project = await createProject(organization.id);
+    const project = await createProjectWithPrompts(organization.id);
     createConfig(project);
     printDashboardLink(project);
   },
 });
 
-async function createProject(organizationId: string) {
+async function createProjectWithPrompts(organizationId: string) {
   const { projectName } = await prompts(
     {
       type: 'text',
@@ -34,11 +34,14 @@ async function createProject(organizationId: string) {
   );
   const creatingSpinner = ora('Creating new project: ' + projectName);
   creatingSpinner.start();
-  const { data: newProject, error: insertError } = await supabase
-    .from('projects')
-    .insert({ name: projectName, organization_id: organizationId, version: 2 })
-    .select('id, name')
-    .single();
+  const { data: newProject, error: insertError } = await createProject({
+    name: projectName,
+    organization_id: organizationId,
+  });
+  // const { data: newProject, error: insertError } = await createProjectViaAPI({
+  //   name: projectName,
+  //   organization_id: organizationId,
+  // });
   if (insertError) {
     creatingSpinner.fail('Error creating project');
     console.error(insertError);
@@ -46,4 +49,52 @@ async function createProject(organizationId: string) {
   }
   creatingSpinner.succeed();
   return { id: newProject.id, name: newProject.name };
+}
+
+async function createProject(args: {
+  name: string;
+  organization_id: string;
+}): Promise<{ data: any; error: any }> {
+  try {
+    return await supabase
+      .from('projects')
+      .insert({
+        name: args.name,
+        organization_id: args.organization_id,
+        version: 2,
+      })
+      .select('id, name')
+      .single();
+  } catch (e) {
+    return { data: undefined, error: e };
+  }
+}
+
+// Eventually use this function to create a project via the API
+async function createProjectViaAPI(args: {
+  name: string;
+  organization_id: string;
+}): Promise<{ data: any; error: any }> {
+  try {
+    const token = (await supabase.auth.getSession()).data?.session.access_token;
+    if (!token) {
+      return { data: undefined, error: 'No access token found' };
+    }
+
+    const res = await fetch('https://api.triplit.io/projects', {
+      body: JSON.stringify(args),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      method: 'POST',
+    });
+    if (!res.ok) {
+      return { data: undefined, error: await res.text() };
+    }
+
+    return { data: await res.json(), error: undefined };
+  } catch (e) {
+    return { data: undefined, error: e };
+  }
 }
