1280e3c20f39fbe32df8d9a120ea78da5073cca7
Will Ernst <wernst@users.noreply.github.com>
2024-04-18 13:54:46 -0700

    record docker image publishes

--- packages/node-server/scripts/docker-check-version-and-publish.js
+++ packages/node-server/scripts/docker-check-version-and-publish.js
@@ -1,5 +1,6 @@
 import semver from 'semver';
-import packageJson from '../package.json' assert { type: 'json' };
+import serverPackageJson from '../package.json' assert { type: 'json' };
+import dbPackageJson from '../../db/package.json' assert { type: 'json' };
 import { execSync } from 'child_process';
 
 // I think this is only the 100 latest published tags, however that should be enough for our purposes
@@ -20,7 +21,7 @@ async function publishIfNewVersion() {
   const imageTags = await getLatestImageTags();
   const versionTags = imageTags.filter((tag) => semver.valid(tag));
   const latestPublishedVersion = semver.maxSatisfying(versionTags, '*');
-  const currentVersion = packageJson.version;
+  const currentVersion = serverPackageJson.version;
 
   if (
     !latestPublishedVersion ||
@@ -30,8 +31,52 @@ async function publishIfNewVersion() {
     // working directory is package root
     execSync('yarn build', { stdio: 'inherit' });
     execSync('yarn publish', { stdio: 'inherit' });
+    await recordImagePublish({
+      server_version: currentVersion,
+      db_version: dbPackageJson.version,
+      image: `aspencloud/triplit-db:${currentVersion}`,
+    });
   } else {
     console.log('Current version is already published. Skipping publish...');
+  }
+}
+
+// args: {server_version: string, db_version: string, image: string}
+async function recordImagePublish(args) {
+  if (!process.env.PROJECTS_ADMIN_API_URL) {
+    console.error(
+      'No PROJECTS_ADMIN_API_URL found in environment, unable to record image publish.'
+    );
+    return;
+  }
+  if (!process.env.PROJECTS_ADMIN_API_KEY) {
+    console.error(
+      'No PROJECTS_ADMIN_API_KEY found in environment, unable to record image publish.'
+    );
+    return;
+  }
+
+  try {
+    const response = await fetch(
+      `${process.env.PROJECTS_ADMIN_API_URL}/server_deploys`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.PROJECTS_ADMIN_API_KEY}`,
+        },
+        body: JSON.stringify(args),
+      }
+    );
+    if (!response.ok) {
+      console.error(
+        'Error recording image publish:',
+        response.status,
+        await response.text()
+      );
+    }
+  } catch (e) {
+    console.error('Error recording image publish:', e.message);
   }
 }
 
