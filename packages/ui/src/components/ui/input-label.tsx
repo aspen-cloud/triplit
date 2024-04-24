a19a60947812fbbb14b899070a05015f7ca80346
pbohlman <phil.l.bohlman.gr@dartmouth.edu>
2024-04-24 13:21:28 -0500

    ui: fixup input label colors

--- packages/ui/src/components/ui/input-label.tsx
+++ packages/ui/src/components/ui/input-label.tsx
@@ -4,6 +4,8 @@ type InputLabelProps = {
 
 export function InputLabel(props: InputLabelProps) {
   return (
-    <p className="text-xs ml-1 mb-1 text-zinc-500 font-medium">{props.value}</p>
+    <p className="text-xs ml-1 mb-1 text-muted-foreground font-medium">
+      {props.value}
+    </p>
   );
 }
