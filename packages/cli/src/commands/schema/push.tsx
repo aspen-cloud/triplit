ecfab14afcd8e1f03cdbb15d7f14d583a4d0c580
Arihan Varanasi <63890951+arihanv@users.noreply.github.com>
2024-05-01 14:40:17 -0500

    fix: use ansis/colors instead of ansi-colors

--- packages/cli/src/commands/schema/push.tsx
+++ packages/cli/src/commands/schema/push.tsx
@@ -1,39 +1,39 @@
-import c from 'ansi-colors';
+import {yellow, blue, red} from 'ansis/colors';
 import { Command } from '../../command.js';
 import { serverRequesterMiddleware } from '../../middleware/add-server-requester.js';
 import { readLocalSchema } from '../../schema.js';
 import { logSchemaChangeViolations, schemaToJSON } from '@triplit/db';
 import ora from 'ora';
-
 export default Command({
-  description: 'Apply the local schema to the server',
-  middleware: [serverRequesterMiddleware],
-  run: async ({ ctx }) => {
-    const localSchema = schemaToJSON({
-      collections: await readLocalSchema(),
-      version: 0,
-    });
-    const pushSpinner = ora('Pushing schema to server').start();
-    let data;
-    try {
-      data = await ctx.requestServer('POST', '/override-schema', {
-        schema: localSchema,
-      });
-      pushSpinner.succeed('Schema pushed to server');
-    } catch (e) {
-      pushSpinner.fail('Failed to push schema to server');
-      data = e;
-    }
-    if (!Object.hasOwn(data, 'successful') || !Object.hasOwn(data, 'issues')) {
-      console.error(data);
-      return;
-    }
-    logSchemaChangeViolations(data.successful, data.issues, {
-      warn: (message, ...args) => console.log(c.yellow(message), ...args),
-      info: (message, ...args) => console.log(c.blue(message), ...args),
-      error: (message, ...args) => console.log(c.red(message), ...args),
-      debug: () => {},
-      scope: () => this,
-    });
-  },
+    description: 'Apply the local schema to the server',
+    middleware: [serverRequesterMiddleware],
+    run: async ({ ctx }) => {
+        const localSchema = schemaToJSON({
+            collections: await readLocalSchema(),
+            version: 0,
+        });
+        const pushSpinner = ora('Pushing schema to server').start();
+        let data;
+        try {
+            data = await ctx.requestServer('POST', '/override-schema', {
+                schema: localSchema,
+            });
+            pushSpinner.succeed('Schema pushed to server');
+        }
+        catch (e) {
+            pushSpinner.fail('Failed to push schema to server');
+            data = e;
+        }
+        if (!Object.hasOwn(data, 'successful') || !Object.hasOwn(data, 'issues')) {
+            console.error(data);
+            return;
+        }
+        logSchemaChangeViolations(data.successful, data.issues, {
+            warn: (message, ...args) => console.log(yellow(message), ...args),
+            info: (message, ...args) => console.log(blue(message), ...args),
+            error: (message, ...args) => console.log(red(message), ...args),
+            debug: () => { },
+            scope: () => this,
+        });
+    },
 });
