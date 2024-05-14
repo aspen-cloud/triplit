a1e4af9bb537212df15a41696ea42001fa240e2a
pbohlman <phil.l.bohlman.gr@dartmouth.edu>
2024-05-14 17:27:52 -0400

    use code snippet styling in sidebar

--- packages/docs/theme.config.jsx
+++ packages/docs/theme.config.jsx
@@ -12,7 +12,17 @@ export default {
     link: 'https://discord.gg/q89sGWHqQ5',
   },
   sidebar: {
-    defaultMenuCollapseLevel: 1,
+    defaultMenuCollapseLevel: 2,
+    titleComponent({ title, type }) {
+      if (title.startsWith('`')) {
+        return (
+          <code className="nx-border-black nx-border-opacity-[0.04] nx-bg-opacity-[0.03] nx-bg-black nx-break-words nx-rounded-md nx-border nx-py-0.5 nx-px-[.25em] nx-text-[.9em] dark:nx-border-white/10 dark:nx-bg-white/10">
+            {title.replace(/`/g, '')}
+          </code>
+        );
+      }
+      return <>{title}</>;
+    },
   },
   docsRepositoryBase:
     'https://github.com/aspen-cloud/triplit/tree/main/packages/docs',
