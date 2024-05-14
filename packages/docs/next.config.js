87bccb7b041d8467d41d70a3b3622ba033571a5b
pbohlman <phil.l.bohlman.gr@dartmouth.edu>
2024-05-14 19:22:43 -0400

    add redirects

--- packages/docs/next.config.js
+++ packages/docs/next.config.js
@@ -11,5 +11,60 @@ module.exports = withNextra({
       destination: '/frameworks/react',
       permanent: false,
     },
+    {
+      source: '/fetching-data/queries',
+      destination: '/query',
+      permanent: true,
+    },
+    {
+      source: '/fetching-data/fetch',
+      destination: '/client/fetch',
+      permanent: true,
+    },
+    {
+      source: '/fetching-data/subscriptions',
+      destination: '/client/subscribe',
+      permanent: true,
+    },
+    {
+      source: '/migrations',
+      destination: '/schemas',
+      permanent: true,
+    },
+    {
+      source: '/access-control',
+      destination: '/schemas/rules',
+      permanent: true,
+    },
+    {
+      source: '/syncing-data/:path*',
+      destination: '/client',
+      permanent: true,
+    },
+    {
+      source: '/updating-data',
+      destination: '/client/insert',
+      permanent: true,
+    },
+    {
+      source: '/schema-management',
+      destination: '/schemas/updating',
+      permanent: true,
+    },
+    {
+      source: '/database/schemas',
+      destination: '/schemas',
+      permanent: true,
+    },
+    {
+      source: '/database/storage',
+      destination: '/client/storage',
+      permanent: true,
+    },
+    {
+      source: '/database',
+      destination: '/client',
+      permanent: true,
+    },
   ],
 });
