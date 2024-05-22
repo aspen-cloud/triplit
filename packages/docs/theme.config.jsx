c7507dcac9fe8cf09de3d37ff714ffbcfc7d508b
pbohlman <phil.l.bohlman.gr@dartmouth.edu>
2024-05-22 12:46:25 -0400

    patch nextra sidebar spacing

--- packages/docs/theme.config.jsx
+++ packages/docs/theme.config.jsx
@@ -17,7 +17,7 @@ export default {
     titleComponent({ title, type }) {
       if (title.startsWith('`')) {
         return (
-          <code className="nx-border-black nx-border-opacity-[0.04] nx-bg-opacity-[0.03] nx-bg-black nx-break-words nx-rounded-md nx-border nx-py-0.5 nx-px-[.25em] nx-text-[.9em] dark:nx-border-white/10 dark:nx-bg-white/10">
+          <code className="nx-border-black nx-border-opacity-[0.04] nx-bg-opacity-[0.03] nx-bg-black nx-break-words nx-rounded-md nx-border nx-py-[1px] nx-px-[.25em] nx-text-[.9em] dark:nx-border-white/10 dark:nx-bg-white/10">
             {title.replace(/`/g, '')}
           </code>
         );
