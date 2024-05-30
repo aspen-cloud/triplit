f2238bdbea5568db58296b9f5b975ceaebdd09c4
pbohlman <phil.l.bohlman.gr@dartmouth.edu>
2024-05-30 13:02:46 -0400

    console: use memory storage for project management

--- packages/console/triplit/client.ts
+++ packages/console/triplit/client.ts
@@ -1,6 +1,5 @@
 import { schemaObject as DBConsoleSchemaObject } from './schema';
 import { TriplitClient } from '@triplit/client';
-import { IndexedDbStorage } from '@triplit/db/storage/indexed-db';
 
 export function formConsolePrimaryKey(projectId: string, server: string) {
   return `${projectId}-${server}`;
@@ -8,8 +7,5 @@ export function formConsolePrimaryKey(projectId: string, server: string) {
 
 export const consoleClient = new TriplitClient({
   schema: DBConsoleSchemaObject,
-  storage: {
-    cache: new IndexedDbStorage('console-cache-v2'),
-    outbox: new IndexedDbStorage('console-outbox-v2'),
-  },
+  storage: 'memory',
 });
