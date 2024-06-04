6d67b0bb209b8f51051fc30796dd965581a3aa96
Will Ernst <wernst@users.noreply.github.com>
2024-06-03 17:10:05 -0700

    add deprecated comment to collectionQuery entityId

--- packages/db/src/query.ts
+++ packages/db/src/query.ts
@@ -133,7 +133,9 @@ export type CollectionQuery<
   order?: QueryOrder<M, CN>[];
   limit?: number;
   after?: [ValueCursor, boolean];
-  // Deprecated: remove this and references on next major version
+  /**
+   * @deprecated define a where filter instead
+   */
   entityId?: string; // Syntactic sugar for where("id", "=", entityId), should not be relied on in query engine
   vars?: Record<string, any>;
   collectionName: CN;
