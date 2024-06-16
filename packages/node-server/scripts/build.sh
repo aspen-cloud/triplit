5f3045c794bf4e05f379f358ebc332892a4ef928
Will Ernst <wernst@users.noreply.github.com>
2024-06-16 17:17:21 -0400

    fixup docker build typo

--- packages/node-server/scripts/build.sh
+++ packages/node-server/scripts/build.sh
@@ -5,5 +5,5 @@ VERSION=$(node -p "require('./package.json').version")
 
 # Build and tag the Docker image
 # Specify platforms: https://docs.docker.com/build/building/multi-platform/#building-multi-platform-images
-docker buildx build -t aspencloud/triplit-server:$VERSION --platform linux/amd64,linux/arm64 --load .
-docker tag aspencloud/triplit-server:$VERSION aspencloud/triplit-server:latest
+docker buildx build -t aspencloud/triplit-server:$VERSION -t aspencloud/triplit-server:latest --platform linux/amd64,linux/arm64 --push .
+# docker tag aspencloud/triplit-server:$VERSION aspencloud/triplit-server:latest
