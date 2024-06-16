4e1329f7c1cf8c9866145c2a033c8344fdd7a398
Will Ernst <wernst@users.noreply.github.com>
2024-06-16 16:23:11 -0400

    setup multi platform docker build

--- packages/node-server/scripts/build.sh
+++ packages/node-server/scripts/build.sh
@@ -4,5 +4,6 @@
 VERSION=$(node -p "require('./package.json').version")
 
 # Build and tag the Docker image
-docker build -t aspencloud/triplit-server:$VERSION .
+# Specify platforms: https://docs.docker.com/build/building/multi-platform/#building-multi-platform-images
+docker build -t aspencloud/triplit-server:$VERSION --platform linux/amd64,linux/arm64 .
 docker tag aspencloud/triplit-server:$VERSION aspencloud/triplit-server:latest
