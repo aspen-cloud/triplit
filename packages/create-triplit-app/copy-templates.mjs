9fa2aa3b2cc04a2da2baaeec8c86ad5e1bc9cff8
Matthew Linkous <matthew.linkous@gmail.com>
2024-06-22 16:30:15 -0500

    cta: Copy templates during build step

---
+++ packages/create-triplit-app/copy-templates.mjs
@@ -0,0 +1,26 @@
+import { copy, remove } from 'fs-extra';
+import { join, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const TEMPLATE_NAMES = ['react', 'svelte'];
+const SOURCE_DIR = '../../templates';
+const DEST_DIR = './dist/templates';
+
+async function copyTemplates() {
+  await remove(DEST_DIR);
+  const pwd = resolve(fileURLToPath(import.meta.url), '../');
+  try {
+    for (const template of TEMPLATE_NAMES) {
+      await copy(
+        resolve(pwd, SOURCE_DIR, template),
+        resolve(pwd, DEST_DIR, template)
+      );
+      console.log(`Copied ${template} to ${DEST_DIR}`);
+    }
+    console.log('Templates copied successfully!');
+  } catch (error) {
+    console.error('An error occurred while copying templates:', error);
+  }
+}
+
+await copyTemplates();
