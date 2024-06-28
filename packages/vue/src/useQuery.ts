31aea7a17fe949daef1e778c1764b0671fa54d5a
Moritz R <35408274+itoxiq@users.noreply.github.com>
2024-06-28 14:16:58 +0200

    feat: added vue bindings

---
+++ packages/vue/src/useQuery.ts
@@ -0,0 +1,104 @@
+import { Ref, computed, ref, watchEffect } from "vue";
+import type {
+  ClientFetchResult,
+  ClientQuery,
+  ClientQueryBuilder,
+  Models,
+  SubscriptionOptions,
+  TriplitClient,
+  Unalias,
+} from "@triplit/client";
+import { WorkerClient } from "@triplit/client/worker-client";
+
+export function useQuery<
+  M extends Models<any, any> | undefined,
+  Q extends ClientQuery<M, any, any, any>
+>(
+  client: TriplitClient<any> | WorkerClient<any>,
+  query: ClientQueryBuilder<Q>,
+  options?: Partial<SubscriptionOptions>
+) {
+  const results = ref<
+    Unalias<ClientFetchResult<Q>> | undefined
+  >(undefined) as Ref<Unalias<ClientFetchResult<Q>> | undefined>;
+  const isInitialFetch = ref(true);
+  const fetchingLocal = ref(false);
+  const fetchingRemote = ref(
+    client.connectionStatus !== "CLOSED"
+  );
+  const fetching = computed(
+    () =>
+      fetchingLocal.value ||
+      (isInitialFetch.value && fetchingRemote.value)
+  );
+  const error = ref<unknown>(undefined);
+  let hasResponseFromServer = false;
+
+  const builtQuery = ref(query && query.build()) as Ref<Q>;
+
+  function updateQuery(query: ClientQueryBuilder<Q>) {
+    builtQuery.value = query.build();
+    results.value = undefined;
+    fetchingLocal.value = true;
+    hasResponseFromServer = false;
+  }
+
+  watchEffect(() => {
+    client
+      .isFirstTimeFetchingQuery(builtQuery.value)
+      .then((isFirstFetch) => {
+        isInitialFetch.value = isFirstFetch;
+      });
+    const unsub = client.onConnectionStatusChange(
+      (status) => {
+        if (status === "CLOSING" || status === "CLOSED") {
+          fetchingRemote.value = false;
+          return;
+        }
+        if (
+          status === "OPEN" &&
+          hasResponseFromServer === false
+        ) {
+          fetchingRemote.value = true;
+          return;
+        }
+      },
+      true
+    );
+    return () => {
+      unsub();
+    };
+  });
+
+  watchEffect(() => {
+    const unsubscribe = client.subscribe(
+      builtQuery.value,
+      (newResults) => {
+        fetchingLocal.value = false;
+        error.value = undefined;
+        results.value = newResults;
+      },
+      (err) => {
+        fetchingLocal.value = false;
+        error.value = err;
+      },
+      {
+        ...(options ?? {}),
+        onRemoteFulfilled: () => {
+          hasResponseFromServer = true;
+          fetchingRemote.value = false;
+        },
+      }
+    );
+    return () => {
+      unsubscribe();
+    };
+  });
+
+  return {
+    fetching,
+    results: computed(() => results.value),
+    error: computed(() => error.value),
+    updateQuery,
+  };
+}
