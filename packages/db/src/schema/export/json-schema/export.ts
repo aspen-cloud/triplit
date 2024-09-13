95fc810238815bc2e6ef9633d032d519de00b698
mentalgear <2837147+MentalGear@users.noreply.github.com>
2024-09-13 18:02:42 +0200

    chore: make immutable

--- packages/db/src/schema/export/json-schema/export.ts
+++ packages/db/src/schema/export/json-schema/export.ts
@@ -52,6 +52,7 @@ export function exportSchemaAsJSONSchema(
 ): JSONSchema7 | undefined {
   //
   if (!schema) return undefined;
+
   const collectionsListJsonSchema: Record<string, JSONSchema7> = {};
 
   const triplitSchemaJsonData = schemaToJSON({
@@ -59,11 +60,16 @@ export function exportSchemaAsJSONSchema(
     version: 0,
   });
 
-  for (const collectionKey in triplitSchemaJsonData?.collections) {
+  // copy and work on duplicate to keep original object unchanged
+  const duplicateOfTriplitSchemaJsonData = structuredClone(
+    triplitSchemaJsonData
+  );
+
+  for (const collectionKey in duplicateOfTriplitSchemaJsonData?.collections) {
     //
     const collectionJsonSchema: JSONSchema7 =
       transformTriplitJsonDataInJsonSchema(
-        triplitSchemaJsonData,
+        duplicateOfTriplitSchemaJsonData,
         collectionKey
       );
 
