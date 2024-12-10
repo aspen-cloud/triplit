b8e7a77e595366cd82670e9bb153b2e958610514
Matthew Linkous <matthew.linkous@gmail.com>
2024-12-10 15:35:59 -0600

    db: skip schema overriding data checks for relational attributes

--- packages/db/src/schema/diff.ts
+++ packages/db/src/schema/diff.ts
@@ -257,6 +257,7 @@ function areDifferent(a: any, b: any): boolean {
 }
 
 type ALLOWABLE_DATA_CONSTRAINTS =
+  | 'none'
   | 'never'
   | 'collection_is_empty'
   | 'attribute_is_empty' // undefined
@@ -266,7 +267,7 @@ type ALLOWABLE_DATA_CONSTRAINTS =
 
 type BackwardsIncompatibleEdits = {
   issue: string;
-  allowedIf: ALLOWABLE_DATA_CONSTRAINTS;
+  dataConstraint: ALLOWABLE_DATA_CONSTRAINTS;
   context: CollectionAttributeDiff;
   attributeCure: (
     collection: string,
@@ -284,7 +285,7 @@ export function getBackwardsIncompatibleEdits(schemaDiff: Diff[]) {
     if (maybeDangerousEdit) {
       acc.push({
         issue: maybeDangerousEdit.description,
-        allowedIf: maybeDangerousEdit.allowedIf,
+        dataConstraint: maybeDangerousEdit.dataConstraint,
         context: curr,
         attributeCure: maybeDangerousEdit.attributeCure,
       });
@@ -297,17 +298,29 @@ const DANGEROUS_EDITS = [
   {
     description: 'removed an optional attribute',
     matchesDiff: (diff: CollectionAttributeDiff) => {
-      return diff.type === 'delete' && diff.metadata.optional === true;
+      return (
+        diff.type === 'delete' &&
+        diff.metadata.type !== 'query' &&
+        diff.metadata.optional === true
+      );
     },
-    allowedIf: 'attribute_is_empty',
+    dataConstraint: 'attribute_is_empty',
     attributeCure: () => null,
   },
   {
-    description: 'removed a non-optional attribute',
+    description: 'removed an relational attribute',
     matchesDiff: (diff: CollectionAttributeDiff) => {
-      return diff.type === 'delete';
+      return diff.type === 'delete' && diff.metadata.type === 'query';
     },
-    allowedIf: 'collection_is_empty',
+    dataConstraint: 'none',
+    attributeCure: () => null,
+  },
+  {
+    description: 'removed a non-relational attribute',
+    matchesDiff: (diff: CollectionAttributeDiff) => {
+      return diff.type === 'delete' && diff.metadata.type !== 'query';
+    },
+    dataConstraint: 'collection_is_empty',
     attributeCure: (_collection, attribute) =>
       `make '${attribute.join('.')}' optional`,
   },
@@ -319,7 +332,7 @@ const DANGEROUS_EDITS = [
       }
       return false;
     },
-    allowedIf: 'attribute_has_no_undefined',
+    dataConstraint: 'attribute_has_no_undefined',
     attributeCure: () => null,
   },
   {
@@ -330,7 +343,7 @@ const DANGEROUS_EDITS = [
       }
       return false;
     },
-    allowedIf: 'attribute_is_empty',
+    dataConstraint: 'attribute_is_empty',
     attributeCure: (_collection, attribute) =>
       `revert the change to '${attribute.join(
         '.'
@@ -344,7 +357,7 @@ const DANGEROUS_EDITS = [
       }
       return false;
     },
-    allowedIf: 'attribute_is_empty',
+    dataConstraint: 'attribute_is_empty',
     attributeCure: (_collection, attribute) =>
       `revert the change to '${attribute.join(
         '.'
@@ -362,7 +375,7 @@ const DANGEROUS_EDITS = [
         return true;
       return false;
     },
-    allowedIf: 'collection_is_empty',
+    dataConstraint: 'collection_is_empty',
     attributeCure: (_collection, attribute) =>
       `make '${attribute.join('.')}' optional`,
   },
@@ -374,7 +387,7 @@ const DANGEROUS_EDITS = [
       }
       return false;
     },
-    allowedIf: 'attribute_has_no_null',
+    dataConstraint: 'attribute_has_no_null',
     attributeCure: () => null,
   },
   {
@@ -386,7 +399,7 @@ const DANGEROUS_EDITS = [
       }
       return false;
     },
-    allowedIf: 'attribute_satisfies_enum',
+    dataConstraint: 'attribute_satisfies_enum',
     attributeCure: (_collection, attribute, enumArray) =>
       `revert the change to '${attribute.join(
         '.'
@@ -395,7 +408,7 @@ const DANGEROUS_EDITS = [
       )} are in the new enum: ${enumArray}`,
   },
 ] satisfies {
-  allowedIf: ALLOWABLE_DATA_CONSTRAINTS;
+  dataConstraint: ALLOWABLE_DATA_CONSTRAINTS;
   description: string;
   matchesDiff: (diff: CollectionAttributeDiff) => boolean;
   attributeCure: (
@@ -435,12 +448,14 @@ export async function getSchemaDiffIssues(
       const violatesExistingData = !(await isEditSafeWithExistingData(
         tx,
         edit.context,
-        edit.allowedIf
+        edit.dataConstraint
       ));
-      const dataCure = DATA_CHANGE_CURES[edit.allowedIf](
-        edit.context.collection,
-        edit.context.attribute
-      );
+      const dataCure =
+        edit.dataConstraint &&
+        DATA_CHANGE_CURES[edit.dataConstraint](
+          edit.context.collection,
+          edit.context.attribute
+        );
       const attributeCure = edit.attributeCure(
         edit.context.collection,
         edit.context.attribute
@@ -448,7 +463,10 @@ export async function getSchemaDiffIssues(
       return {
         ...edit,
         violatesExistingData,
-        cure: attributeCure ? attributeCure + ' or ' + dataCure : dataCure,
+        cure:
+          attributeCure && dataCure
+            ? attributeCure + ' or ' + dataCure
+            : dataCure,
       };
     })
   );
@@ -465,6 +483,7 @@ const DATA_CONSTRAINT_CHECKS: Record<
   ) => Promise<boolean>
 > = {
   never: async () => false,
+  none: async () => true,
   collection_is_empty: detectCollectionIsEmpty,
   attribute_is_empty: detectAttributeIsEmpty,
   attribute_has_no_undefined: detectAttributeHasNoUndefined,
@@ -477,6 +496,8 @@ const DATA_CHANGE_CURES: Record<
   (collection: string, attribute: string[]) => string
 > = {
   never: () => 'This edit is never allowed',
+  none: () =>
+    'This edit does not violate any data constraints but can cause existing queries that reference this attribute to fail',
   collection_is_empty: (collection) =>
     `delete all entities in '${collection}' to allow this edit`,
   attribute_is_empty: (_collection, attribute) =>
