a5b5c5fe0001a9f2c1edc24a997c53bae978d14f
Matthew Linkous <matthew.linkous@gmail.com>
2025-03-13 16:53:32 -0500

    db: use corejs polyfills

---
+++ .changeset/lemon-avocados-search.md
@@ -0,0 +1,5 @@
+---
+'@triplit/db': patch
+---
+
+Add core-js polyfills
--- packages/db/src/polyfills.ts
+++ packages/db/src/polyfills.ts
@@ -1,18 +1,5 @@
 // @ts-nocheck
-if (typeof Promise.withResolvers === 'undefined') {
-  Promise.withResolvers = function () {
-    let resolve, reject;
-    const promise = new Promise((res, rej) => {
-      resolve = res;
-      reject = rej;
-    });
-    return { promise, resolve, reject };
-  };
-}
-
-if (typeof Symbol.dispose === 'undefined') {
-  Symbol.dispose = Symbol('Symbol.dispose');
-}
-if (typeof Symbol.asyncDispose === 'undefined') {
-  Symbol.asyncDispose = Symbol('Symbol.asyncDispose');
-}
+import 'core-js/proposals/iterator-helpers';
+import 'core-js/proposals/promise-with-resolvers';
+import 'core-js/actual/symbol/async-dispose';
+import 'core-js/actual/symbol/dispose';
