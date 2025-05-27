719e153e60b0e9b4672c623626ba3fd07494daf1
pbohlman <phil.l.bohlman.gr@dartmouth.edu>
2025-05-27 16:35:31 -0400

    cli: filter out default roles from schema diff command output

--- packages/cli/src/commands/schema/diff.tsx
+++ packages/cli/src/commands/schema/diff.tsx
@@ -1,4 +1,4 @@
-import { yellow } from 'ansis/colors';
+import { blue, yellow } from 'ansis/colors';
 import { Command } from '../../command.js';
 import { createServerRequesterMiddleware } from '../../middleware/add-server-requester.js';
 import { projectSchemaMiddleware } from '../../middleware/project-schema.js';
@@ -34,11 +34,23 @@ export default Command({
     if (!localSchema) console.log(yellow`Local project is schemaless`);
     if (!serverSchema) console.log(yellow`Remote database is schemaless`);
     if (localSchema && serverSchema) {
-      const diff =
+      let diff =
         flags.ahead === 'local'
           ? diffSchemas(serverSchema, localSchema)
           : diffSchemas(localSchema, serverSchema);
+      // If the local schema doesn't have roles defined
+      // the remote server will have the DEFAULT_ROLES
+      // but that's not really a difference that the user
+      // can control or should care about
+      if (!localSchema.roles)
+        diff = diff.filter(({ _diff }) => _diff !== 'roles');
       // TODO: prettify diff output
+      if (diff.length === 0) {
+        console.log(
+          blue('No differences found between local and remote schema.')
+        );
+        return;
+      }
       console.dir(diff, { depth: null });
     }
   },
